f46cbd2b17ee156a2694f315c82acf522690dd8a
tienthienhd <tienthienhd@gmail.com>
2023-04-14 10:43:13 +0700

    init

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@RestController
+	public static class TestController {
+		@GetMapping("/")
+		public ResponseEntity<String> check(){
+			return ResponseEntity.ok("test");
+		}
+	}
+}
+
